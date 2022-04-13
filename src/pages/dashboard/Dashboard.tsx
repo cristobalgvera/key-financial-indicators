2fcddc9a9433e90361b836ae2de60752815280c0
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2022-04-12 21:12:16 -0400

    feat(dashboard): created base dashboard layout

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -4,8 +4,10 @@ import { FinancialIndicators } from '@features/financial-indicator/components';
 export const Dashboard = () => {
   const { data } = useGetIndicatorsQuery();
   return (
-    <div data-testid="dashboard">
-      <h1>Dashboard</h1>
+    <div data-testid="dashboard" className="grid gap-y-4">
+      <div className="flex justify-center">
+        <h1 className="text-3xl font-bold text-principal-500">Dashboard</h1>
+      </div>
       <FinancialIndicators indicators={data ?? []} />
     </div>
   );
